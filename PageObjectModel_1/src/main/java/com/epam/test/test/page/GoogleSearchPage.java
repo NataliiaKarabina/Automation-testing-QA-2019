eb6e64646ea921f11968b239f27089e7b172c995
NataliiaKarabina <47696877+NataliiaKarabina@users.noreply.github.com>
2019-04-21 19:10:12 +0300

    Add files via upload

--- PageObjectModel_1/src/main/java/com/epam/test/test/page/GoogleSearchPage.java
+++ PageObjectModel_1/src/main/java/com/epam/test/test/page/GoogleSearchPage.java
@@ -1,9 +1,11 @@
 package com.epam.test.test.page;
 
 import org.openqa.selenium.WebElement;
-
+//Create a class containing methods where the locator values
+// are declared without any attribute names
 public class GoogleSearchPage {
-    private WebElement q;
+    private WebElement q;  //Here's the Element q is declared without stating
+    // any locator type. However, it automatically identifies whether it is id or name.
 
     public void searchFor(String text) {
         q.sendKeys(text);
